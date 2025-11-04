b18d69ce4657a270791c32496dd99a54d8eb7e3a
Coyotexy <leonardobrandaolealdiniz@gmail.com>
2025-11-04 14:11:39 -0300

    Add files via upload

---
+++ TCM 2/hamburguer.js
@@ -0,0 +1,11 @@
+function abreFecha() {
+
+    let sm = document.getElementById("submenu")
+    if (sm.style.display == "block") {
+        sm.style.display = "none";
+    } 
+    else {
+        sm.style.display = "block";
+    }
+
+}
